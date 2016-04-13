0e463be4e954eb9f52566eaaacec0c5b4a13b54b
稻米鼠 <i@zji.me>
2016-04-13 13:16:45 +0800

    代码整理

--- JavaScript/Markdms.js
+++ JavaScript/Markdms.js
@@ -3,7 +3,8 @@
 function Markdms()
 {
     // this.firstname=firstname;
-    // 代码的处理
+    // -------- 代码的处理 --------
+    // 下面的函数可以将代码中的特殊字符替换为实体符号
     function htmlEncode(str){
         var i,s={
             "&amp;": /&/g,
@@ -20,22 +21,35 @@ function Markdms()
         }
         return str;
     }
+    // 代码高亮函数，此处只做简单的处理，如果需要高级的处理和自行引入代码高亮插件
+    this.codehighlight = codehighlight;
+    function codehighlight(code,lang) {
+        code = htmlEncode(code);
+        if(lang != null && lang !=""){
+            lang = ' lang="'+lang+'"';
+        }
+        code = '<pre'+lang+'>'+code+'</pre>';
+        return code
+    }
+    // 定义代码处理方法
     this.code = code;
     function code(input)
     {
+        // 获取所有的代码块，存入数组 codeArray
         var codeArray = input.match(/```[\s\S]*?```/g);
+        // 获取所有的非代码块，存入数组 noCodeArray
         var noCodeArray = input.split(/```[\s\S]*?```/g);
+        // 假如说存在代码块，则对代码会进行相应的处理
         if (codeArray != null){
             for (var index = 0; index < codeArray.length; index++) {
+                // 获取语言设置
                 var codefirstline = codeArray[index].match(/```.*\n/);
                 var lang = codefirstline[0].replace(/```\s{0,}/,"").replace(/\s{0,}\n/,"");
+                // 获取代码块中的代码
                 codeArray[index] = codeArray[index].replace(/```.*\n/,"```");
                 codeArray[index] = codeArray[index].replace(/```([\s\S]*?)\n{0,}```/g,"$1");
-                codeArray[index] = htmlEncode(codeArray[index]);
-                if(lang != null && lang !=""){
-                    lang = ' lang="'+lang+'"';
-                }
-                codeArray[index] = '<pre'+lang+'>'+codeArray[index]+'</pre>';
+                // 调用代码高亮函数
+                codeArray[index] = this.codehighlight(codeArray[index],lang);
             }
         }
         // 合并字符串并输出
